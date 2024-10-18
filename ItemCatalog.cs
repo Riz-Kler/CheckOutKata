0d60546e65b728a24772bd880134f911f827b10e
Riz Kler <rizwan.kler@gmail.com>
2024-10-18 02:08:15 +0100

    Added catalog of Items

---
+++ ItemCatalog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    public class ItemCatalog
+    {
+        private List<Item> catalog = new List<Item>();
+
+        public ItemCatalog UpdateItemUnitPrice(string sku, int itemprice)
+        {
+            this.catalog.RemoveAll(x => x.Sku == sku);
+            this.catalog.Add(new Item(sku, itemprice));
+            return this;
+        }
+
+        public ItemCatalog RemoveItem(string sku)
+        {
+            this.catalog.RemoveAll(x => x.Sku == sku);
+            return this;
+        }
+
+        public int GetUnitPriceForItem(string sku)
+        {
+            var item = this.catalog.Where(x => x.Sku == sku).FirstOrDefault();
+            if (item == null) throw new UnknownSkuException(sku);
+            return item.UnitPrice;
+        }
+    }
+
+   
+}
