2e9fd4358498ddff2287031eb51679e5f3d0b131
Riz Kler <rizwan.kler@gmail.com>
2024-10-18 03:53:21 +0100

    Failing Test to checkout single item.

--- CheckoutKataTests/CheckoutTests.cs
+++ CheckoutKataTests/CheckoutTests.cs
@@ -16,5 +16,18 @@ namespace CheckoutKataTests
                     .SpecialPriceItemUnit("B", 2, 45);
             checkout = new Checkout(catalog, specialprices);
         }
+           
+           [Fact]
+            public void price_of_a_single_item_A()
+            {
+                // Arrange                                                           
+                var checkout = new Checkout();
+
+                // Act                                                               
+                checkout.Scan("A");
+
+                // Assert                                                            
+                Assert.Equal(50, checkout.Total);
+            } 
     }
 }
