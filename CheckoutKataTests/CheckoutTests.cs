295e951889d318d183a19f415cc72e81066b55e7
Riz Kler <rizwan.kler@gmail.com>
2024-10-18 04:40:06 +0100

    Failed Test for any item. SHould have pushed earlier

--- CheckoutKataTests/CheckoutTests.cs
+++ CheckoutKataTests/CheckoutTests.cs
@@ -42,5 +42,22 @@ namespace CheckoutKataTests
                 // Assert                                                            
                 Assert.Equal(30, checkout.Total);
             }
+            [Theory]
+            [InlineData('A', 50)]
+            [InlineData('B', 30)]
+            [InlineData('C', 20)]
+            [InlineData('D', 15)]
+            public void price_of_any_single_item(string sku, int expected_total)
+            {
+                // Arrange                                                           
+                //var checkout = new Checkout();
+
+                // Act                                                               
+                checkout.Scan(sku);
+
+                // Assert                                                            
+                Assert.Equal(expected_total, checkout.Total);
+            }
+
     }
 }
